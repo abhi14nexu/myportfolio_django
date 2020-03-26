9d8d099e3ddbb2516a6f5ce75110f133f8232640
Abhishek Yadav <abhishek.yadav14112000@gmail.com>
2020-03-27 02:20:21 +0530

    this is the first commit after jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class blogs(models.Model):
+   title=models.CharField(max_length=200)
+   pub_date=models.DateField()
+   pic=models.ImageField(upload_to='images/')
+   body=models.TextField()
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#create A blog models
+   #title
+   #pub_date
+   #body 
+   #image
+
+#add the blog app to the settings
+
+#create a migration
+
+#migrate
+
+#Add to the admin
+
+
+
--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import settings
+from django.conf.urls.static import static
 
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+import jobs.views #important method
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',jobs.views.home,name='home'),
 ]
 # + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
